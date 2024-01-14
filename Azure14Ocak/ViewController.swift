467b8e2cf3567a0070f9d647362f33d70c3a2a40
erdi ergene <erdieergene@gmail.com>
2024-01-14 12:17:04 +0300

    Initial Commit

---
+++ Azure14Ocak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Azure14Ocak
+//
+//  Created by Erdi Ergene on 14.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
