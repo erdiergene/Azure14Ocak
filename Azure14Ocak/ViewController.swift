316e292993e8eed64339a86ced63a429433439d1
erdi ergene <erdieergene@gmail.com>
2024-01-14 15:46:18 +0300

    Test git mirror

--- Azure14Ocak/ViewController.swift
+++ Azure14Ocak/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("Test git mirror")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
